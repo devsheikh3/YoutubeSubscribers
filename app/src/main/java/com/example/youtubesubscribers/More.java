8729aa2a414847c8291568b5c125efb37714598f
devsheikh3 <devsheikh3@gmail.com>
2020-06-04 08:21:53 +0500

    USub

--- app/src/main/java/com/example/youtubesubscribers/More.java
+++ app/src/main/java/com/example/youtubesubscribers/More.java
@@ -3,19 +3,73 @@ package com.example.youtubesubscribers;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 
 public class More extends Fragment {
+
+    TextView name,email,invite,feedback,logout,policy,rate;
+    CircleImageView dp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_more, container, false);
+
+        name=view.findViewById(R.id.channelnameid);
+        email=view.findViewById(R.id.emailid);
+        invite=view.findViewById(R.id.inviteFriendsid);
+        feedback=view.findViewById(R.id.feedbackid);
+        logout=view.findViewById(R.id.logoutid);
+        policy=view.findViewById(R.id.privatePolicyid);
+        rate=view.findViewById(R.id.rateAppid);
+        dp=view.findViewById(R.id.channeldpid);
+
+        invite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
 
 
 
